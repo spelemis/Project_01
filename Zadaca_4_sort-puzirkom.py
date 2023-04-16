a0804a899c80b403d0e2f135f7e4bf2beff66e62
Sasa <spelemis@mail.ru>
2023-04-17 00:36:10 +0300

    modified:   Zadaca_4_sort-puzirkom.py

--- Zadaca_4_sort-puzirkom.py
+++ Zadaca_4_sort-puzirkom.py
@@ -1,15 +1,20 @@
 #Отсортировать рандомный целочисленный массив из 10 чисел в интервале от (1; 100) 
 #Любым из пройденных способов
+# Это программа будет переделана на сорт. пузырком .
 import random
-i=0
+
 Massiv=[0,0,0,0,0,0,0,0,0,0]
-for i in range (0,9):
+i, j = (0, 0)
+# DlinaMassiva = 0
+DlinaMassiva = len (Massiv)
+for i in range (0, len (Massiv)):
     Massiv[i]= random.randint(1,100)
 print ('Несортированный массив:', Massiv) 
+i=0
 while i<DlinaMassiva:
     Min=Massiv[i]
     IndexMin=i
-    for j in range(i,DlinaMassiva):
+    for j in range(0,DlinaMassiva):
         if Massiv[j]<Min:
             Min= Massiv[j]
             IndexMin=j
