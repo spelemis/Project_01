07f7d3957b905d601819477bf8e5460d5f0ba50c
Sasa <spelemis@mail.ru>
2023-05-07 21:06:24 +0300

    Изменены названия переменных на английские.

--- task_1.2.py
+++ task_1.2.py
@@ -6,6 +6,8 @@
 # Выведите общее время звучания трех случайных песен в формате
 # Три песни звучат ХХХ минут
 
+print(' ----------- A -----------------\n')
+
 my_favorite_songs = [
     ['Waste a Moment', 3.03],
     ['New Salvation', 4.02],
@@ -20,22 +22,22 @@ my_favorite_songs = [
 
 import random
 
-DlinaMassiva= len(my_favorite_songs)
-print ('\n Длина списка: ', DlinaMassiva,'\n')
+Array_length= len(my_favorite_songs)
+print ('\n Длина списка: ', Array_length,'\n')
 print ('Список песень: \n')
 i=0
-while i<DlinaMassiva:
+while i<Array_length:
     print ('          №',i+1, my_favorite_songs[i][0], my_favorite_songs[i][1] )
     i +=1
 i=0
 X=0   # Ощее время звучания
 print('\n Случайно выбраные песни (сонги): \n')
 while i<3:
-    VibraniySong= random.randint (0, DlinaMassiva-1)
-    X=X + my_favorite_songs[VibraniySong][1]       # Считаем общее время звучания
+    Selected_song= random.randint (0, Array_length-1)
+    X=X + my_favorite_songs[Selected_song][1]       # Считаем общее время звучания
 
-    print('           i=', VibraniySong+1,'/Название:', my_favorite_songs[VibraniySong][0], 
-          '/Длина звучания сонга:', my_favorite_songs[VibraniySong][1])
+    print('           i=', Selected_song+1,'/Название:', my_favorite_songs[Selected_song][0], 
+          '/Длина звучания сонга:', my_favorite_songs[Selected_song][1])
     i +=1
 print ('\n Три песни звучат', X,'мин',
        '\n*****************************************\n')
@@ -44,6 +46,8 @@ print ('\n Три песни звучат', X,'мин',
 # Есть словарь песен 
 # Распечатайте общее время звучания трех случайных песен
 # Вывод: Три песни звучат ХХХ минут.
+
+print(' ----------- B -----------------\n')
 
 my_favorite_songs_dict = {
     'Waste a Moment': 3.03,
@@ -56,25 +60,23 @@ my_favorite_songs_dict = {
     'Nowhere to Run': 2.58,
     'In This World': 4.02,
 }
-DlinaMassiva= len(my_favorite_songs_dict)
+Array_length= len(my_favorite_songs_dict)
 
-print ('\n Длина словаря: ', DlinaMassiva,'\n')
+print ('\n Длина словаря: ', Array_length,'\n')
 i=0
-X=0   # Ощее время звучания
+X=0   # Общее время звучания
 print('         Случайно выбраные песни (сонги): \n')
 while i<3:
-    VibraniySong= random.randint (0, DlinaMassiva-1)
+    Selected_song= random.randint (0, Array_length-1)
     j=0
     for key in my_favorite_songs_dict:
-        if j==VibraniySong :
+        if j==Selected_song :
             print('          j=',j+1, key, my_favorite_songs_dict[key])
             X +=my_favorite_songs_dict[key]
         j +=1
     i +=1
 print ('\n Три песни звучат ', X,'мин',
        '\n------------------------------------------\n')
-
-       
 
 
 # Дополнительно для пунктов A и B
@@ -83,22 +85,36 @@ print ('\n Три песни звучат ', X,'мин',
 # import random
 # УЖЕ СДЕЛАНО В ПУНКТАХ А и В !!!
 
+print(' -----------C.A-----------------\n')
+
+print('         Случайно выбранная песня : \n')
+Selected_song= random.randint (0, Array_length-1)
+print (Selected_song+1,'.', my_favorite_songs[Selected_song])
+
+
+print(' -----------C.B-----------------\n')
+
+print('случайно выбранная песня:', random.sample(my_favorite_songs, 1) )
+
+
 
 # Дополнительно 
 # Пункт D.
 # Переведите минуты и секунды в формат времени. Используйте модуль datetime 
 
+print(' ----------- D -----------------\n')
+
 import datetime
 
-Zvucanie= datetime.timedelta()
+Duration= datetime.timedelta()
 
-print ('\n Длина списка: ', DlinaMassiva,'\n')
+print ('\n Длина списка: ', Array_length,'\n')
 print ('Список песень: \n')
 i=0
-while i<DlinaMassiva:
-    Zv=round(my_favorite_songs[i][1]*60)
-    Zvucanie=datetime.timedelta( seconds=Zv)
-    print ('       №',i+1, my_favorite_songs[i][0], Zvucanie)
+while i<Array_length:
+    Dur=round(my_favorite_songs[i][1]*60)
+    Duration=datetime.timedelta( seconds=Dur)
+    print ('       №',i+1, my_favorite_songs[i][0], Duration)
     i +=1
 
 
