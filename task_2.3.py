c8b54592c4845ed75d687815184243135d0211f4
Sasa <spelemis@mail.ru>
2023-05-07 23:08:28 +0300

    Изменены наименования переменных на английский язык.

--- task_2.3.py
+++ task_2.3.py
@@ -13,10 +13,11 @@ def switch_it_up(number):
     if number not in range (0,9):
         return None
     else:
-        CifriPropisyu={1:'один',2:'два', 3:'три', 4:'четыри', 5:'пять', 6:'шесть', 7:'семь',8:'восемь', 9:'девять', 0:'нуль'}
-        return CifriPropisyu[number]
+        Numbers_in_words={1:'один',2:'два', 3:'три', 4:'четыри', 5:'пять', 6:'шесть', 7:'семь',8:'восемь', 9:'девять', 0:'нуль'}
+        return Numbers_in_words[number]
 
-Cifra=int(input('Введите цифру от 1 по 9 :'))
+Number=int(input('Введите цифру от 0 по 9 :'))
 
-print ('Вы ввели цифру:',switch_it_up(Cifra) )
+print ('Вы ввели цифру:',switch_it_up(Number) )
+print ('\n')
 
