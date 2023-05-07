9aa27a8922c927a5ccda56d8aad8ec18401a7365
Sasa <spelemis@mail.ru>
2023-05-07 23:54:16 +0300

    Изменено предсталение вывода на экран монитора.

--- Zadaca_6.1_Salary.py
+++ Zadaca_6.1_Salary.py
@@ -6,26 +6,33 @@
 # Формат вывода:
 # Сотрудник накопит: ХХХ.ХХ рублей
 
-salary=float(input('Внесите сумму зарплаты:'))
-expenses = float(input('Внесите сумму расходов:'))
+salary=float(input('\n     Внесите сумму зарплаты:'))
+expenses = float(input('     Внесите сумму расходов:'))
 money_acc=float(0)
 salary_1 = salary
 salary_2 = salary
 
 # 1. Вариант
 # Зарплата увеличивается 5% по отношению с предыдущим месяцом 
+print('\n----------- 1.Вариант----------')
+print('Зарплата увеличивается 5% по отношению с предыдущим месяцом\n')
 for i in range(12) :
     money_acc += salary - expenses
-    print (i,'  Zarplata:', round(salary,2), 'Rashodi:', expenses, 'Nakopljenjije:', round(money_acc,2))
+    print (i,'  Зарплата:', round(salary,2), 'Расходы:', expenses, 'Накопление:', round(money_acc,2))
     salary *= 1.05
 print ('Сотрудник накопит:', round(money_acc,2),'руб')
 
 
 # 2. Вариант
 # Зарплата увеличивается 5% по отношению с первым месяцом
+
+print('\n------- 2. Вариант-----------')
+print('Зарплата увеличивается 5% по отношению с первым месяцом\n')
 money_acc = 0 
 Inc_salary= salary_1 * 0.05
 for i in range(12) :
     money_acc += salary_2 - expenses
+    print (i,'  Зарплата:', round(salary_2,2), 'Расходы:', expenses, 'Накопление:', round(money_acc,2))
     salary_2 += Inc_salary
 print ('\n Сотрудник накопит:', round(money_acc,2),'руб')
+print('\n')
