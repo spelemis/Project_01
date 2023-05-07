0e35caa7fbdabfa385c57c17c276d09d60bd66f5
Sasa <spelemis@mail.ru>
2023-05-07 20:44:34 +0300

    Изменены названия переменных на английские.

--- task_1.1.py
+++ task_1.1.py
@@ -10,20 +10,20 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 Number_of_letters = len(my_favorite_songs) # Определение длины входной строки
 i=0   # Счётчик букв в входной строке
 j=0   # Счётчик треков
-Pesnya=['','','','']      # Множество треков выделенных из входной строки
-Song=''   # Промежуточное название трека
+Song=['','','','']      # Множество треков выделенных из входной строки
+Temp_Song=''   # Промежуточное название трека
 while i < Number_of_letters:
     if my_favorite_songs[i] == ',': 
-        Pesnya[j]=Song # Выделили запятую, значит название трека собрано .
+        Song[j]=Temp_Song # Выделили запятую, значит название трека собрано .
         j=j+1
         i=i+1     #Пробел после запятой в входной строке перескочить.
-        Song =''
-    Song = Song + my_favorite_songs[i] # Собираем название трека № j из букв.
+        Temp_Song =''
+    Temp_Song = Temp_Song + my_favorite_songs[i] # Собираем название трека № j из букв.
     i=i+1
 # Выводим данные в заданной очереди :
     
-print('\n','\n', Pesnya[0], Pesnya[3], Pesnya[1], Pesnya[2])
-print('\n',Pesnya[0],'\n', Pesnya[3],'\n', Pesnya[1],'\n', Pesnya[2])
+print('\n','\n', Song[0], Song[3], Song[1], Song[2])
+print('\n',Song[0],'\n', Song[3],'\n', Song[1],'\n', Song[2])
 
 
     
